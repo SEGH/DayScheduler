9d8491629af30865148b86c4bf872005c84ba5d5
Susan Holland <segh@fastmail.com>
2020-06-13 12:53:00 -0400

    Local Storage Functions

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignments
 var today = moment().format("dddd, MMMM Do");
-var todayString = moment();
 var now = moment().startOf('hour');
 var todayTime = moment().format("YYYY-MM-DD");
 var textArea = $(".description");
+var textArray = [];
 
 // Set currentDay to a moment representing today
 $("#currentDay").text(today);
@@ -16,17 +16,37 @@ function timeColor() {
 
         if (moment(time).isBefore(now)) {
             $(textArea[i]).addClass("past");
-        }
-        if (moment(time).isSame(now)) {
+        } else if (moment(time).isSame(now)) {
             $(textArea[i]).addClass("present");
-        } 
-        if (moment(time).isAfter(now)) {
+        } else if (moment(time).isAfter(now)) {
             $(textArea[i]).addClass("future");
         }
     }
 }
 timeColor();
 
-// Event listener for Save buttons
+// Event listener for Save buttons with anon function to save descriptions to local storage
+$(".saveBtn").on("click", function() {
+    var hour = $(this).siblings(".description")[0].name;
+    var date = todayTime + hour;
+    var text = $(this).siblings(".description")[0].value;
+    var textObject = { date: date, hour: hour, description: text };
+    textArray.push(textObject);
+    localStorage.setItem("tasks", JSON.stringify(textArray));
+});
 
-// Function to save descriptions to local storage
+// Function to display saved description text
+function renderText() {
+    var tempText = JSON.parse(localStorage.getItem("tasks"));
+    if (tempText !== null) {
+        textArray = tempText;
+        textArray.forEach(function(obj) {
+            for (var i = 0; i < textArea.length; i++) {
+                if (textArea[i].name == obj.hour) {
+                    textArea[i].value = obj.description;
+                }
+            }
+        });
+    }
+}
+renderText();
