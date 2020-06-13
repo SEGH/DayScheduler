3e50fa6512469fca31776f2f25e2495a9f584107
Susan Holland <segh@fastmail.com>
2020-06-13 11:19:31 -0400

    Added JS pseudocode and html textarea names

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Assignments
 var today = moment().format("dddd, MMMM Do");
+var todayString = moment();
+var now = moment().startOf('hour');
+var todayTime = moment().format("YYYY-MM-DD");
+var textArea = $(".description");
 
+// Set currentDay to a moment representing today
 $("#currentDay").text(today);
+
+// Function to dynamically change description background color based on time
+
+// Event listener for Save buttons
+
+// Function to save descriptions to local storage
