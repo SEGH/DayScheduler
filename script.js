0191b354484a38d50f1aef38406d50f5cb79eee0
Susan Holland <segh@fastmail.com>
2020-06-13 16:32:15 -0400

    Added tomorrow functionality

--- script.js
+++ script.js
@@ -2,18 +2,49 @@
 var today = moment().format("dddd, MMMM Do");
 var now = moment().startOf('hour');
 var todayTime = moment().format("YYYY-MM-DD");
+var tomorrow = moment().add(1, 'days').format("dddd, MMMM Do");
+var tomorrowTime = moment().add(1, 'days').format("YYYY-MM-DD");
+var displayDay = "today";
 var textArea = $(".description");
 var textArray = [];
 
-// Set currentDay to a moment representing today
-$("#currentDay").text(today);
+// Set currentDay to a moment representing today or tomorrow
+function setPage() {
+    if (displayDay === "today") {
+        $("#currentDay").text(today);
+    } else if (displayDay === "tomorrow") {
+        $("#currentDay").text(tomorrow);
+    }
+}
+setPage();
+
+// Event Listeners for page turners
+$(".next").on("click", function() {
+    displayDay = "tomorrow";
+    setPage();
+    renderText();
+    timeColor();
+});
+
+$(".prev").on("click", function() {
+    displayDay = "today";
+    setPage();
+    timeColor();
+    renderText();
+})
 
 // Function to dynamically change description background color based on time
 function timeColor() {
     for (var i = 0; i < textArea.length; i++) {
-
-        var time = todayTime + textArea[i].name;
-
+        $(textArea[i]).removeClass("past present future");
+        var time;
+        if (displayDay === "today") {
+            time = todayTime + textArea[i].name;
+        } else if (displayDay === "tomorrow") {
+            time = tomorrowTime + textArea[i].name;
+        }
+        console.log(displayDay);
+        console.log(time);
         if (moment(time).isBefore(now)) {
             $(textArea[i]).addClass("past");
         } else if (moment(time).isSame(now)) {
@@ -28,7 +59,13 @@ timeColor();
 // Event listener for Save buttons with anon function to save descriptions to local storage
 $(".saveBtn").on("click", function() {
     var hour = $(this).siblings(".description")[0].name;
-    var date = todayTime + hour;
+    var date;
+    if (displayDay === "today") {
+        date = todayTime + hour;
+    } else if (displayDay === "tomorrow") {
+        date = tomorrowTime + hour;
+    }
+
     var text = $(this).siblings(".description")[0].value;
     var textObject = { date: date, hour: hour, description: text };
     textArray.push(textObject);
@@ -37,12 +74,20 @@ $(".saveBtn").on("click", function() {
 
 // Function to display saved description text
 function renderText() {
+    textArea.val("");
     var tempText = JSON.parse(localStorage.getItem("tasks"));
     if (tempText !== null) {
         textArray = tempText;
         textArray.forEach(function(obj) {
             for (var i = 0; i < textArea.length; i++) {
-                if (textArea[i].name == obj.hour) {
+                var date;
+                if (displayDay === "today") {
+                    date = todayTime + obj.hour;
+                } else if (displayDay === "tomorrow") {
+                    date = tomorrowTime + obj.hour;
+                }
+
+                if ((textArea[i].name == obj.hour) && (date === obj.date)) {
                     textArea[i].value = obj.description;
                 }
             }
